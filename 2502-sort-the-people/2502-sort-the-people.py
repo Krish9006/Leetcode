e0af6bb7c29cf9e519617be42fed101f8d21e5bc
krish gupta <148880964+Krish9006@users.noreply.github.com>
2024-07-22 21:40:07 +0530

    Time: 134 ms (30.08%), Space: 17.2 MB (14.55%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,9 +1,14 @@
 from typing import List
+
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        indices = list(range(len(names)))
-        def height(index):
-            return heights[index]
-        indices.sort(key=height, reverse=True)
-        sorted_names = [names[i] for i in indices]
+        sorted_heights = sorted(heights, reverse=True)
+        sorted_names = []
+        while sorted_heights:
+            max_height = sorted_heights.pop(0)
+            index = heights.index(max_height)
+            sorted_names.append(names[index])
+            heights.pop(index)
+            names.pop(index)
+        
         return sorted_names
