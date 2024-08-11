fbf8cad57efecc73ce10762f89a5c4f4256a80dc
krish gupta <148880964+Krish9006@users.noreply.github.com>
2024-08-11 20:55:20 +0530

    Time: 0 ms (100%), Space: 43.6 MB (46.82%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -2,19 +2,19 @@ class Solution {
     public boolean isValidBST(TreeNode root) {
         return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
-    
+
     private boolean isValidBST(TreeNode node, long minValue, long maxValue) {
         if (node == null) {
             return true;
         }
-        
+
         if (node.val <= minValue || node.val >= maxValue) {
             return false;
         }
-        
+
         boolean leftIsValid = isValidBST(node.left, minValue, node.val);
         boolean rightIsValid = isValidBST(node.right, node.val, maxValue);
-        
+
         return leftIsValid && rightIsValid;
     }
 }
