7fcb2ff316ed1cb0efc8214bd9a5e389cce1958f
krish gupta <148880964+Krish9006@users.noreply.github.com>
2024-08-29 21:20:21 +0530

    Time: 415 ms (5%), Space: 44.7 MB (44.61%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean exists(TreeNode root, TreeNode target) {
-        if (root == null) return false;
-        if (root == target) return true;
+        if (root == null)
+            return false;
+        if (root == target)
+            return true;
         return exists(root.left, target) || exists(root.right, target);
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) return root;
-        
+        if (root == null || root == p || root == q)
+            return root;
+
         boolean leftP = exists(root.left, p);
         boolean leftQ = exists(root.left, q);
 
@@ -17,6 +20,6 @@ class Solution {
         if (leftP == false && leftQ == false) {
             return lowestCommonAncestor(root.right, p, q);
         }
-        return root; 
+        return root;
     }
 }
