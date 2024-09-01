379caf2ebe3e539e7ca4ad72deac181e702e49cc
krish gupta <148880964+Krish9006@users.noreply.github.com>
2024-09-01 12:43:48 +0530

    Time: 0 ms (100%), Space: 44.3 MB (43.49%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return checkBalance(root) != -1;
+    }
+
+    private int checkBalance(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftHeight = checkBalance(node.left);
+        if (leftHeight == -1) return -1;
+
+        int rightHeight = checkBalance(node.right);
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
