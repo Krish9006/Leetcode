43c551ea28831b415f744562e5099267f71760fe
krish gupta <148880964+Krish9006@users.noreply.github.com>
2024-09-02 20:13:14 +0530

    Time: 6 ms (63.92%), Space: 45.3 MB (40.44%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        int n = asteroids.length;
+
+        for (int i = 0; i < n; i++) {
+            int current = asteroids[i];
+            while (!stack.isEmpty() && current < 0 && stack.peek() > 0) {
+                if (stack.peek() < -current) {
+                    stack.pop();
+                } else if (stack.peek() == -current) {
+                    stack.pop();
+                    current = 0;
+                    break;
+                } else {
+                    current = 0;
+                    break;
+                }
+            }
+            
+            if (current != 0) {
+                stack.push(current);
+            }
+        }
+
+        int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+}
