8a280f3631113a9b199983f86c7fe134dd104955
krish gupta <148880964+Krish9006@users.noreply.github.com>
2024-10-18 20:36:59 +0530

    Time: 4 ms (87.49%), Space: 44.4 MB (40.48%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,15 +1,24 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] sorted = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(sorted);
         int[] result = new int[nums.length];
-        for(int i = 0; i < nums.length; i++) {
-            int count = 0;
-            for(int j = 0; j < nums.length; j++) {
-                if(nums[j] < nums[i]) {
-                    count++;
-                }
-            }
-            result[i] = count;
+        
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = findIndex(sorted, nums[i]);
         }
+        
         return result;
+    }
+
+    private int findIndex(int[] sorted, int target) {
+        for (int i = 0; i < sorted.length; i++) {
+            if (sorted[i] == target) {
+                return i;
+            }
+        }
+        return 0;
     }
 }
