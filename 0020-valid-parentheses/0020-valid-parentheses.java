6bb348db1704a80a4d95baf0923f53ac5255808a
krish gupta <148880964+Krish9006@users.noreply.github.com>
2025-02-25 22:10:57 +0530

    Time: 2 ms (97.67%), Space: 42.4 MB (6.51%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (!stack.isEmpty() && (
+                (c == ')' && stack.peek() == '(') ||
+                (c == ']' && stack.peek() == '[') ||
+                (c == '}' && stack.peek() == '{')
+            )) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
