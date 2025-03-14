2e754249686f7fd73fcc95d485e5a4e2ad6fba3f
krish gupta <148880964+Krish9006@users.noreply.github.com>
2025-03-14 20:47:00 +0530

    Time: 3 ms (99.89%), Space: 45.1 MB (83.24%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,20 +1,14 @@
-import java.util.*;
-
 class Solution {
-    static boolean ans = true; // Initialize ans properly
-    
+    static boolean ans = true; 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int n = numCourses;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        
         for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
-
         for (int[] pre : prerequisites) {
             adj.get(pre[1]).add(pre[0]);
         }
-
         boolean[] visited = new boolean[n];
         boolean[] path = new boolean[n];
         for (int i = 0; i < n; i++) {
