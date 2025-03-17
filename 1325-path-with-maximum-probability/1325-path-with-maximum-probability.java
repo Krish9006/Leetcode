407d043089a8c60b5cab403a1a75c882c668afc8
krish gupta <148880964+Krish9006@users.noreply.github.com>
2025-03-17 13:10:11 +0530

    Time: 25 ms (84.08%), Space: 58.7 MB (20.9%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -1,42 +1,43 @@
 class Solution {
-    class pair implements Comparable<pair> {
+    class Pair implements Comparable<Pair> {
         int node;
-        double prob;
-        pair(int node, double prob) {
+        double prob; 
+        Pair(int node, double prob) {
             this.node = node;
             this.prob = prob;
         }
-        public int compareTo(pair p) {
+        public int compareTo(Pair p) {
+            if (this.prob == p.prob) return this.node - p.node;
             return Double.compare(p.prob, this.prob); 
         }
     }
-    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        List<List<pair>> adj = new ArrayList<>();
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        List<List<Pair>> adj = new ArrayList<>();
         for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
         for (int i = 0; i < edges.length; i++) {
             int u = edges[i][0], v = edges[i][1];
             double prob = succProb[i];
-            adj.get(u).add(new pair(v, prob));
-            adj.get(v).add(new pair(u, prob)); 
+            adj.get(u).add(new Pair(v, prob));
+            adj.get(v).add(new Pair(u, prob)); 
         }
         double[] prob = new double[n];
-        Arrays.fill(prob, 0.0);
-        prob[start_node] = 1.0; 
-        PriorityQueue<pair> pq = new PriorityQueue<>();
-        pq.add(new pair(start_node, 1.0));
+        Arrays.fill(prob, 0);
+        prob[start] = 1; 
+        PriorityQueue<Pair> pq = new PriorityQueue<>(); 
+        pq.add(new Pair(start, 1));
         while (!pq.isEmpty()) {
-            pair top = pq.poll();
+            Pair top = pq.poll();
             int node = top.node;
             double currProb = top.prob;
-            if (node == end_node) return currProb;
-            for (pair p : adj.get(node)) {
+            if (node == end) return currProb;
+            for (Pair p : adj.get(node)) {
                 double newProb = currProb * p.prob;
                 if (newProb > prob[p.node]) {
                     prob[p.node] = newProb;
-                    pq.add(new pair(p.node, newProb));
+                    pq.add(new Pair(p.node, newProb));
                 }
             }
         }
-        return 0.0; 
+        return 0; 
     }
 }
