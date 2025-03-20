83625c19b1d34eb2b0b3c6646c05a463035f1cee
krish gupta <148880964+Krish9006@users.noreply.github.com>
2025-03-20 12:57:27 +0530

    Time: 0 ms (100%), Space: 40.2 MB (79.94%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,20 +1,14 @@
-
-
 class Solution {
     public int fib(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return fiber(n, dp);
+        int []dp= new int[n+1];
+        Arrays.fill(dp,-1);
+        return slove(n,dp);
+        
     }
-
-    private int fiber(int n, int[] dp) {
-        if (n <= 1) {
-            return n;
-        }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        dp[n] = fiber(n - 1, dp) + fiber(n - 2, dp);
-        return dp[n];
+    private int slove(int i,int[]dp){
+        if(i<=1)return i;
+        if(dp[i]!=-1)return dp[i];
+        dp[i]=slove(i-1,dp)+slove(i-2,dp);
+        return dp[i];
     }
 }
