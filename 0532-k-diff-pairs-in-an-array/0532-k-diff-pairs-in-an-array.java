6e20e964b0ca0522e19a2c91b963e9d8b51d0ce1
krish gupta <148880964+Krish9006@users.noreply.github.com>
2025-03-23 12:43:32 +0530

    Time: 10 ms (71.67%), Space: 45.4 MB (19.8%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -1,32 +1,19 @@
-import java.util.Arrays;
+import java.util.HashSet;
 
 public class Solution {
     public int findPairs(int[] nums, int k) {
-        if (k < 0) return 0;
-        Arrays.sort(nums);
-        int i = 0, j = 1, count = 0;
-
-        while (j < nums.length) {
-            int diff = nums[j] - nums[i];
-            if (i == j || diff < k) {
-                j++;
-            } else if (diff > k) {
-                i++;
-            } else {
-                count++;
-                i++;
-                j++;
-                while (i < nums.length && nums[i] == nums[i - 1]) i++;
-                while (j < nums.length && nums[j] == nums[j - 1]) j++;
+        HashSet<Integer> seen = new HashSet<>();
+        HashSet<Integer> unseen = new HashSet<>();
+        
+        for (int num : nums) {
+            if (seen.contains(num - k)) {
+                unseen.add(num - k);
             }
+            if (seen.contains(num + k)) {
+                unseen.add(num);
+            }
+            seen.add(num);
         }
-        return count;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.findPairs(new int[]{3, 1, 4, 1, 5}, 2)); // Output: 2
-        System.out.println(solution.findPairs(new int[]{1, 2, 3, 4, 5}, 1)); // Output: 4
-        System.out.println(solution.findPairs(new int[]{1, 3, 1, 5, 4}, 0)); // Output: 1
+        return unseen.size();
     }
 }
