79a01633ea921be047fa9756aa172f0e80c9bd35
krish gupta <148880964+Krish9006@users.noreply.github.com>
2025-04-12 12:05:05 +0530

    Time: 0 ms (100%), Space: 45.5 MB (93.31%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,28 +2,36 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
-        boolean[] zeroRows = new boolean[rows];
-        boolean[] zeroCols = new boolean[cols];
+        boolean firstRowZero = false, firstColZero = false;
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    zeroRows[i] = true;
-                    zeroCols[j] = true;
-                }
-            }
-        }
-        for (int i = 0; i < rows; i++) {
-            if (zeroRows[i]) {
-                for (int j = 0; j < cols; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
+            if (matrix[i][0] == 0) firstColZero = true;
         }
         for (int j = 0; j < cols; j++) {
-            if (zeroCols[j]) {
-                for (int i = 0; i < rows; i++) {
+            if (matrix[0][j] == 0) firstRowZero = true;
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+        if (firstRowZero) {
+            for (int j = 0; j < cols; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (firstColZero) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
