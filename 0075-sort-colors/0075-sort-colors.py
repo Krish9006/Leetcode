b094cbefbafc460b6d78e120474b4aa3f8e409dc
krish gupta <148880964+Krish9006@users.noreply.github.com>
2025-04-14 14:14:45 +0530

    Time: 0 ms (100%), Space: 17.7 MB (83.3%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,18 +1,12 @@
-from typing import List
-
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         no_of_zeroes = 0
         no_of_ones = 0
-        
- 
         for i in range(len(nums)):
             if nums[i] == 0:
                 no_of_zeroes += 1
             elif nums[i] == 1:
                 no_of_ones += 1
-        
-      
         for i in range(len(nums)):
             if i < no_of_zeroes:
                 nums[i] = 0
