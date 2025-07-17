81826628460a53b1e3efa0ff8138241404ba8188
krish gupta <148880964+Krish9006@users.noreply.github.com>
2025-07-17 23:30:24 +0530

    Time: 1 ms (81.92%), Space: 41.4 MB (16.14%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        int num = Math.abs(x);  
+        
+        int rev = 0;  
+        
+        while (num != 0) {
+            int ld = num % 10;  
+            if (rev > (Integer.MAX_VALUE - ld) / 10) {
+                return 0;
+            }
+            
+            rev = rev * 10 + ld;
+            num = num / 10;  
+        }
+        
+        return (x < 0) ? (-rev) : rev;
+    }
+}
