cc45b0446eb770437394b99f2ac786d96487fe37
krish gupta <148880964+Krish9006@users.noreply.github.com>
2025-08-25 18:05:19 +0530

    Time: 2 ms (99.41%) | Memory: 47.5 MB (26.23%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -9,13 +9,31 @@ class Solution {
             ans[k++] = mat[r][c];
 
             if (up) { // going up-right
-                if (c == n - 1) { r++; up = false; }   // right edge
-                else if (r == 0) { c++; up = false; }  // top edge
-                else { r--; c++; } // normal move
+                if (c == n - 1) {
+                    r++;
+                    up = false;
+                } // right edge
+                else if (r == 0) {
+                    c++;
+                    up = false;
+                } // top edge
+                else {
+                    r--;
+                    c++;
+                } // normal move
             } else { // going down-left
-                if (r == m - 1) { c++; up = true; }    // bottom edge
-                else if (c == 0) { r++; up = true; }   // left edge
-                else { r++; c--; } // normal move
+                if (r == m - 1) {
+                    c++;
+                    up = true;
+                } // bottom edge
+                else if (c == 0) {
+                    r++;
+                    up = true;
+                } // left edge
+                else {
+                    r++;
+                    c--;
+                } // normal move
             }
         }
         return ans;
